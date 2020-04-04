045a690b13b7d6772213ab61e6bf26d320446748
BernhardBaumrock <office@baumrock.com>
2020-04-04 11:43:41 +0200

    add rounding tweaks

--- RockPrice.php
+++ RockPrice.php
@@ -6,32 +6,43 @@ class RockPrice extends WireData {
   public $gross;
 
   private $factor;
+  private $digits;
 
-  public function __construct($net = 0, $tax = 0) {
+  public function __construct($net = 0, $tax = 0, $digits = null) {
+    $this->setDigits($digits);
     $this->setNet($net);
     $this->setTax($tax);
     $this->setGross();
   }
 
+  public function round($val) {
+    return round($val*1, $this->digits);
+  }
+
+  public function setDigits($digits) {
+    if($digits === null) $digits = InputfieldRockPrice::defaultDigits;
+    $this->digits = $digits;
+  }
+
   public function setTax($val) {
     $val = (float)$val;
-    $this->tax = $val;
+    $this->tax = $this->round($val);
     $this->factor = 1+($val/100);
     $this->setVat();
     $this->setGross();
   }
 
   public function setVat() {
-    $this->vat = $this->net * $this->tax/100;
+    $this->vat = $this->round($this->net * $this->tax/100);
   }
 
   public function setNet($val) {
-    $this->net = (float)$val;
+    $this->net = $this->round($val);
   }
 
   public function setGross($val = null) {
-    if($val !== null) $this->gross = $val;
-    else $this->gross = $this->net * $this->factor;
+    if($val !== null) $this->gross = $this->round($val);
+    else $this->gross = $this->round($this->net * $this->factor);
   }
 
   /**
